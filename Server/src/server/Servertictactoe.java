afd8da9b06e654ee1b8b706418b51a08ad14f138
hagermagdy1 <94796014+hagermagdy1@users.noreply.github.com>
2022-01-14 15:24:48 +0200

    piechart of server
    piechart of tottal player number and online players number

--- Server/src/server/Servertictactoe.java
+++ Server/src/server/Servertictactoe.java
@@ -1,32 +1,68 @@
+
 package server;
 
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Side;
+import javafx.scene.Group;
 import javafx.scene.chart.AreaChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
+import static javafx.scene.layout.Region.USE_PREF_SIZE;
 import javafx.stage.Stage;
+import static server.Handler.avaliablePlayers;
 
-public class Servertictactoe extends AnchorPane {
+
+public  class Servertictactoe extends AnchorPane {
 
     protected final ToggleButton startServer;
-    protected final CategoryAxis categoryAxis;
-    protected final NumberAxis numberAxis;
-    protected final AreaChart areaChart;
-    private Stage mystage;
+    protected final PieChart piechart;
+     private Stage mystage;
     ServerSocket serverSocket;
-
     public Servertictactoe(Stage stage) {
+        
+            startServer = new ToggleButton();
+            ObservableList<PieChart.Data> pieChartData = null;
+            piechart = new PieChart(pieChartData);
 
-        startServer = new ToggleButton();
-        categoryAxis = new CategoryAxis();
-        numberAxis = new NumberAxis();
-        areaChart = new AreaChart(categoryAxis, numberAxis);
+        try {
+             int tottalPlayer =DAO.numberOfPlayers();
+             int avilablePlayer=Handler.clientsVector.size();
+             int offlinePlayer=tottalPlayer-avilablePlayer;
+            pieChartData = FXCollections.observableArrayList(
+                    
+                new PieChart.Data("Tottal Players" , DAO.numberOfPlayers())); //totaal in database
+               // new PieChart.Data("Avilable Players",onlinevector )); //onlinevector
+               // new PieChart.Data("Tottalll players" , 6), //totaal in database
+              //  new PieChart.Data("Avilable players",5 ),
+                
+                piechart.setLabelLineLength(50); 
+                piechart.setLegendVisible(true);
+                piechart.setLabelLineLength(50);
+                piechart.setLegendSide(Side.LEFT);
+              
+            //onlinevector
+            System.out.println(Handler.clientsVector.size());
+            System.out.println(DAO.numberOfPlayers());
+             piechart.setClockwise(true); 
+             piechart.setTitle(" Chart Of Players"); 
+
+           //piechart.setLabelsVisible(true); 
+            piechart.setData(pieChartData);
+        } catch (SQLException ex) {
+            Logger.getLogger(Servertictactoe.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
 
         setMaxHeight(USE_PREF_SIZE);
         setMaxWidth(USE_PREF_SIZE);
@@ -40,18 +76,16 @@ public class Servertictactoe extends AnchorPane {
         startServer.setMnemonicParsing(false);
         startServer.setText("Server ON");
 
-        categoryAxis.setSide(javafx.geometry.Side.BOTTOM);
-
-        numberAxis.setSide(javafx.geometry.Side.LEFT);
-        areaChart.setLayoutX(129.0);
-        areaChart.setLayoutY(209.0);
-        areaChart.setPrefHeight(162.0);
-        areaChart.setPrefWidth(268.0);
+        piechart.setLayoutX(189.0);
+        piechart.setLayoutY(200.0);
+        piechart.setPrefHeight(200.0);
+        piechart.setPrefWidth(210.0);
 
         getChildren().add(startServer);
-        getChildren().add(areaChart);
+        getChildren().add(piechart);
 
-        if (startServer.getText() == "Server ON") {
+        
+                if (startServer.getText() == "Server ON") {
             startServer.setText("Server OFF");
             new Thread(new Runnable() {
                 @Override
